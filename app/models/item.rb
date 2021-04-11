165fe18cd96ffe1dcda6d80580ccbd3391b0e021
keita-nakamura30 <keita.618.73@gmail.com>
2021-04-11 18:31:22 +0900

    commit2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   has_one_attached :image
-
+  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_date
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :sales_status
   belongs_to :category
+ 
+
 
   #ジャンルの選択が「--」の時は保存できないようにする
   with_options numericality: { other_than: 0 } do
@@ -21,6 +23,9 @@ class Item < ApplicationRecord
    validates :item_info
    validates :image
    validates :price
-  end
+   end
+   #numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999,message: 'Out of setting range' }
+   validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999,message:' Out of setting range'}
 end
+
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @item = FactoryBot.build(:item)
+  end 
+
+  describe '商品情報の入力' do
+    context '商品情報の入力がうまくいくとき' do
+      it '全ての値が入力されていると登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品情報の入力がうまくいかない時' do
+      it 'item_nameが空だと登録できない' do
+        @item.user = nil 
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include ('User must exist')
+      end
+    
+      it 'imageが空だと登録できない' do
+        @item.image = nil  
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'item_infoが空だと登録できない' do
+        @item.item_info = ''  
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Item info can't be blank"
+      end
+      it 'categoryが0だと登録できない' do
+        @item.category_id = 0 
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Category must be other than 0"
+      end
+      it 'sales_statusが0だと登録できない' do
+        @item.sales_status_id = 0  
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Sales status must be other than 0"
+      end
+      it 'delivery_feeが0だと登録できない' do
+        @item.delivery_fee_id = 0 
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Delivery fee must be other than 0"
+      end
+      it 'prefectureが0だと登録できない' do
+        @item.prefecture_id = 0  
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Prefecture must be other than 0"
+      end
+      it 'delivery_dateが0だと登録できない' do
+        @item.delivery_date_id = 0  
+        @item.valid?                                   
+        expect(@item.errors.full_messages).to include "Delivery date must be other than 0"
+      end
+      it 'priceが半角数字では無い時登録出来ない' do
+        @item.price = 'あいうえおか'  
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price  Out of setting range"
+      end
+      it 'priceが¥299以下の時登録出来ない' do
+        @item.price = 299  
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price  Out of setting range"
+      end
+      it 'priceが¥10000000以上の時登録出来ない' do
+        @item.price = 10,000,000  
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price  Out of setting range"
+      end
+    end
+  end
 end

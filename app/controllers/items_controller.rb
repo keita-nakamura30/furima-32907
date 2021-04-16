3f43b3a459fc9dd06f5c407521e31a5f0a07f292
keita-nakamura30 <keita.618.73@gmail.com>
2021-04-16 17:32:18 +0900

    commit10

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,34 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  only: [:new,:create]
+  before_action :move_to_index, except: [:index]
 
- def index
-  @items 
- end
-
- def new
-  @item = Item.new
-end
-
-def create
-  @item = Item.new(item_params)
-  if @item.save
-    redirect_to root_path
-  else 
-    render :new
+  def index
+    @items = Item.all
   end
-end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
+  end
 
 
-private
+  private
 
   def item_params
     params.require(:item).permit(:image,:category_id, :sales_status_id,:prefecture_id,:delivery_date_id,:item_info,:item_name,:delivery_fee_id,:price).merge(user_id: current_user.id)
-end
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end

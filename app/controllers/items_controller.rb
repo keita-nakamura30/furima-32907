a0f933b376628062bf3e97793febf927145ba742
keita-nakamura30 <keita.618.73@gmail.com>
2021-04-23 11:56:07 +0900

    Revert "Merge pull request #5 from keita-nakamura30/商品一覧表示機能"
    This reverts commit fe568f9da0f3a0e30030b04ae22b82ede6ffac79, reversing
    changes made to a1be148daa6ff29386e0ecdad136b471ebe759f3.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,36 +1,27 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  only: [:new,:create]
-  before_action :move_to_index, except: [:index,:show]
 
-  def index
-    @items = Item.all.order("created_at DESC")
-  end
+ def index
+  @items 
+ end
 
-  def new
-    @item = Item.new
-  end
+ def new
+  @item = Item.new
+end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else 
-      render :new
-    end
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else 
+    render :new
   end
- 
-  def show
-    @item =Item.find(params[:id])
-  end
+end
 
-  private
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-  
+
+private
+
   def item_params
     params.require(:item).permit(:image,:category_id, :sales_status_id,:prefecture_id,:delivery_date_id,:item_info,:item_name,:delivery_fee_id,:price).merge(user_id: current_user.id)
-  end
+end
 end

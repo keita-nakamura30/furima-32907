ed657fbdd53c993f0c9068b051d3a2202268e2a0
keita-nakamura30 <keita.618.73@gmail.com>
2021-05-07 21:46:27 +0900

    commit9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  only: [:new,:create,:edit,:user]
-  before_action :set_item, only:[:show, :edit,:update]
+  before_action :set_item, only:[:show, :edit,:update,:destroy]
   before_action :move_to_index,only:[:edit,:update]
 
   def index
@@ -22,6 +22,11 @@ class ItemsController < ApplicationController
  
   def show
     
+  end
+  
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def edit
